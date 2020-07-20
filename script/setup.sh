3a33069d338f07e2279713f8383a12a8d1489370
ankitmittal <1997ankitmittal@gmail.com>
2020-07-20 12:21:00 +0530

    Create setup.sh

---
+++ script/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+
+
+
+
+
+
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH=$JAVA_HOME/bin:$PATH
+
+source ~/.profile
+
+echo $JAVA_HOME
+
+
+
+
+
+node -v
+
+npm -v
